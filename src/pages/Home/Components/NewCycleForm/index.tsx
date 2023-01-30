b9be4bf6298fe2a39de6c07a060e8007a76bd39e
Lucas Nogueira <lucasandrade_52@hotmail.com>
2023-01-30 17:40:36 -0300

    Feature: implement forms controlled and otimized cycle forms

--- src/pages/Home/Components/NewCycleForm/index.tsx
+++ src/pages/Home/Components/NewCycleForm/index.tsx
@@ -1,6 +1,12 @@
-import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+import { useContext } from "react";
+import { useFormContext } from "react-hook-form";
+import { CyclesContext } from "../../../../contexts/CyclesContext";
 
 export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext);
+  const { register } = useFormContext();
+
   return (
     <FormContainer>
       <label htmlFor="task">Vou trabalhar em</label>
@@ -8,15 +14,17 @@ export function NewCycleForm() {
         id="task"
         list="task-suggestions"
         placeholder="Dê um nome para o seu projeto"
+        disabled={!!activeCycle}
+        {...register("task")}
       />
 
       <datalist id="task-suggestions">
-        <option value="Projeto 1" />
-        <option value="Projeto 2" />
-        <option value="Projeto 3" />
+        <option value="Estudar" />
+        <option value="Trabalhar em um projeto" />
+        <option value="Pausa para um café" />
       </datalist>
 
-      <label htmlFor="minutesAmount">Durante</label>
+      <label htmlFor="minutesAmount">durante</label>
       <MinutesAmountInput
         type="number"
         id="minutesAmount"
@@ -24,9 +32,11 @@ export function NewCycleForm() {
         step={5}
         min={5}
         max={60}
+        disabled={!!activeCycle}
+        {...register("minutesAmount", { valueAsNumber: true })}
       />
 
       <span>minutos.</span>
     </FormContainer>
-  )
+  );
 }
